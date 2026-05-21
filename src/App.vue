c5a22aee9457f7050eec0b954a313f87764e7eae
IrfanAqila31 <irfanaqila31@gmail.com>
2026-05-21 10:33:28 +0800

    latihan: warna teks dinamis menggunakan class dan style inline

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
+import MainButton from './components/MainButton.vue'
 import LatihanListRendering from './components/LatihanListRendering.vue'
+import { ref } from 'vue'
+
+const disabled = ref(false)
+
+const toggleDisable = () => {
+  disabled.value = !disabled.value
+}
 </script>
 
 <template>
   <LatihanListRendering />
+  <MainButton @click="toggleDisable" :disabled="disabled" />
 </template>
 
 <style></style>
--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -31,6 +31,9 @@ const props = defineProps({
     default: false,
   },
 })
+
+const disableColor = '#000'
+const availableColor = '#fff'
 </script>
 
 <template>
@@ -44,7 +47,30 @@ const props = defineProps({
   <!-- <button v-bind="buttonAttributes">{{ handleActive() }}</button> -->
 
   <!-- menggunakan props -->
-  <button :disabled="disabled">{{ props.title }}</button>
+  <!-- <button :disabled="disabled">{{ props.title }}</button> -->
+
+  <!-- mencoba menggunakan class dan style inline dinamis -->
+  <button
+    :disabled
+    class="button"
+    :class="{ disabled: disabled }"
+    :style="{ color: disabled ? disableColor : availableColor }"
+  >
+    {{ props.title }}
+  </button>
 </template>
 
-<style scoped></style>
+<style scoped>
+.button {
+  background-color: #42b983;
+  border: none;
+
+  padding: 10px 20px;
+  border-radius: 20px;
+}
+
+.disabled {
+  opacity: 0.3;
+  background-color: grey;
+}
+</style>
