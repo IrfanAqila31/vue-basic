7684026e4bcb12ddbc1778aea85600805d08922f
IrfanAqila31 <irfanaqila31@gmail.com>
2026-05-25 21:48:25 +0800

    belajar: mencoba form handling

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import MainButton from './components/MainButton.vue'
 import LatihanListRendering from './components/LatihanListRendering.vue'
 import { ref } from 'vue'
+import { chai } from 'globals'
 
 // const disabled = ref(false)
 
@@ -16,6 +17,13 @@ const count = ref(0)
 const increment = (counter) => {
   count.value = count.value + counter
 }
+
+// mencoba form handling
+const nama = ref('')
+const desc = ref('')
+// const check = ref(false)
+// const check = ref([])
+const check = ref('')
 </script>
 
 <template>
@@ -36,6 +44,44 @@ const increment = (counter) => {
   <!-- mencoba inheritance -->
 
   <MainButton class="button saya" @click="increment(5)" />
+  <br /><br />
+  <!-- mencoba form handling -->
+
+  <!-- default -->
+  <!-- <input type="text" :value="nama" @input="(event) => (nama = event.target.value)" /> -->
+
+  <!-- menggunakan v-model menggunakan input -->
+  <!-- <input type="text" v-model="nama" /> -->
+  <input type="text" v-model.lazy="nama" />
+
+  <p>{{ nama }}</p>
+  <br />
+  <!-- menggunakan v-model menggunakan text area -->
+
+  <textarea v-model="desc" />
+  <!-- white-space: pre-line digunakan agar mengikuti dengan inputann dari text areanya -->
+  <p style="white-space: pre-line">{{ desc }}</p>
+  <br />
+
+  <!-- menggunakan v-model menggunakan checkbox -->
+  <!-- <input type="checkbox" v-model="check" /> -->
+  <!-- <input type="checkbox" v-model="check" value="irfan" />
+  <label>irfan</label>
+  <input type="checkbox" v-model="check" value="aqila" />
+  <label>aqila</label> -->
+  <!-- <p>{{ check }}</p> -->
+  <input type="radio" v-model="check" value="irfan" />
+  <label>irfan</label>
+  <input type="radio" v-model="check" value="aqila" />
+  <label>aqila</label>
+
+  <p>{{ check }}</p>
+
+  <!-- <ul>
+    <li v-for="item in check" :key="item">
+      {{ item }}
+    </li>
+  </ul> -->
 </template>
 
 <style></style>
